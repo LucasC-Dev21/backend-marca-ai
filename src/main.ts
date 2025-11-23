73c3b8ef2c1036df5b46c5dc272b0937b2fc11b5
Lucas Cesar Silva Oliveira <lucascesardev2023@gmail.com>
2025-11-23 17:07:23 -0300

    AMA-1 Adiciona arquivo de teste de integração

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'X-Requested-With',
-      'Accept',
-      'Origin',
-    ],
-  });
 
   app.useGlobalPipes(
     new ValidationPipe({
